fb5d8b885b1b3976ab8bc42a353ca80ea1ed8d90
bubstar45 <bubstar45@users.noreply.github.com>
2026-04-11 18:47:56 +0000

    Remove temporary data files

--- convert_utf8.py
+++
@@ -1,7 +0,0 @@
-with open('data.json', 'rb') as f:
-    content = f.read()
-
-with open('data_utf8.json', 'w', encoding='utf-8') as f:
-    f.write(content.decode('utf-8', errors='ignore'))
-
-print("File created: data_utf8.json")
