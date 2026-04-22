ff26cc746be0f47e96be87bb414babef9665f79d
bubstar45 <bubstar45@users.noreply.github.com>
2026-04-22 13:04:26 +0000

    Add back reportlab and playwright

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,48 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
+from django.db.models import Q
 from .models import Post
 
 def blog_list(request):
     posts = Post.objects.filter(is_published=True)
-    return render(request, 'blog/list.html', {'posts': posts})
+    
+    # Category filter
+    category = request.GET.get('category')
+    if category:
+        posts = posts.filter(category=category)
+    
+    # Search
+    query = request.GET.get('q')
+    if query:
+        posts = posts.filter(
+            Q(title__icontains=query) |
+            Q(content__icontains=query) |
+            Q(excerpt__icontains=query)
+        )
+    
+    # Pagination
+    paginator = Paginator(posts, 9)
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
+    
+    context = {
+        'posts': posts,
+        'active_category': category,
+        'query': query,
+    }
+    return render(request, 'blog/list.html', context)
 
 def blog_detail(request, slug):
     post = get_object_or_404(Post, slug=slug, is_published=True)
-    return render(request, 'blog/detail.html', {'post': post})
+    
+    # Related posts
+    related_posts = Post.objects.filter(
+        category=post.category,
+        is_published=True
+    ).exclude(id=post.id)[:3]
+    
+    context = {
+        'post': post,
+        'related_posts': related_posts,
+    }
+    return render(request, 'blog/detail.html', context)
